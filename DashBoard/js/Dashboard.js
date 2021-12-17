ec59bc68d543dc53f2e0acd14c65166c89dfdc12
Nassar, Tamer <tamer.nassar@intel.com>
2021-12-17 21:58:10 +0200

    Construction connection pipe

--- DashBoard/js/Dashboard.js
+++ DashBoard/js/Dashboard.js
@@ -2,13 +2,22 @@
 let Dashboard = {
 
     display_machines: (machines)=>{
+        let container = document.getElementById("overview_container");
+        let machine_leading = document.getElementById("machine_leading");
+        container.removeChild(machine_leading);
+
         machines.map((machine)=>{
-            console.log(machine);
+            let name = machine["name"];
+            let state = machine["state"];
+
+            let machine_container = document.createElement("div");
+            machine_container.innerHTML = name;
+            machine_container.className = "report_card machine_container state_"+state;
+            container.appendChild(machine_container);
         });
     },
 
     init: ()=>{
-
         const onConnection = ()=>{
             console.log("connected");
             NetworkAdapter.send({
